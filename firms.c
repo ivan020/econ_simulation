338d0ea868243232cc57b9a88a15b2db62053bc5
ivan020 <ivevdm@gmail.com>
2025-06-13 18:58:31 +0100

    updated firms struct

--- firms.c
+++ firms.c
@@ -5,4 +5,10 @@ struct Firm {
   int name;
   float w; // wage at the firm
   float m; // liquidity level available;
+
+  // inventory level at firms
+  float inv_min;
+  float inv_max;
 };
+
+// will try dvorak layour
